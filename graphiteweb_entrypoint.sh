1a0876c445931d2bd5f6bb3437f109c9a42e8e50
Vladimir Kolobaev <vkolobaev@avito.ru>
2018-09-04 12:33:46 +0300

    graphite-clickhouse, carbonapi, bioyino, grafana

--- graphiteweb_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# disable daemons
-rm -rf /etc/service/carbon
-rm -rf /etc/service/carbon-aggregator
-rm -rf /etc/service/statsd
-
-exec /sbin/my_init
--- init.sql
+++ init.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS default.graphite_reverse  ( 
+CREATE DATABASE IF NOT EXISTS graphite;
+
+CREATE TABLE IF NOT EXISTS graphite.data ( 
   Path String,  
   Value Float64,  
   Time UInt32,  
@@ -6,27 +8,35 @@ CREATE TABLE IF NOT EXISTS default.graphite_reverse  (
   Timestamp UInt32
 ) ENGINE = GraphiteMergeTree(Date, (Path, Time), 8192, 'graphite_rollup');
 
-CREATE TABLE IF NOT EXISTS default.graphite_tree (
+CREATE TABLE IF NOT EXISTS graphite.data_reverse ( 
+  Path String,  
+  Value Float64,  
+  Time UInt32,
+  Date Date,
+  Timestamp UInt32
+) ENGINE = GraphiteMergeTree('graphite_rollup') PARTITION BY Date ORDER BY (Path, Time) SETTINGS index_granularity = 8192;
+
+CREATE TABLE IF NOT EXISTS graphite.metrics ( 
   Date Date,
   Level UInt32,
   Path String,
   Deleted UInt8,
   Version UInt32
-) ENGINE = ReplacingMergeTree(Date, (Level, Path), 1024, Version);
+) ENGINE = ReplacingMergeTree(Date, (Level, Path), 8192, Version);
 
-CREATE TABLE IF NOT EXISTS default.graphite_series_reverse (
+CREATE TABLE IF NOT EXISTS graphite.series ( 
   Date Date,
   Level UInt32,
   Path String,
   Deleted UInt8,
   Version UInt32
-) ENGINE = ReplacingMergeTree(Date, (Level, Path, Date), 1024, Version);
+) ENGINE = ReplacingMergeTree(Date, (Level, Path, Date), 8192);
 
-CREATE TABLE IF NOT EXISTS default.graphite_tagged (
+CREATE TABLE IF NOT EXISTS graphite.tagged (
   Date Date,
   Tag1 String,
   Path String,
   Tags Array(String),
   Version UInt32,
   Deleted UInt8
-) ENGINE = ReplacingMergeTree(Date, (Tag1, Path, Date), 1024, Version);
+) ENGINE = ReplacingMergeTree(Date, (Tag1, Path, Date), 8192, Version);
