e02ea19cd84c6fe52c80118900bcbef6a23ba532
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2021-10-05 18:15:21 +0200

    Update ClickHouse, optimize tables' schemes

--- init.sql
+++ init.sql
@@ -1,28 +1,28 @@
 CREATE TABLE IF NOT EXISTS default.graphite_reverse (
-  Path String,  
-  Value Float64,  
-  Time UInt32,  
-  Date Date,  
-  Timestamp UInt32
+  Path String CODEC(ZSTD(3)),
+  Value Float64 CODEC(Gorilla, LZ4),
+  Time UInt32 CODEC(DoubleDelta, LZ4),
+  Date Date CODEC(DoubleDelta, LZ4),
+  Timestamp UInt32 CODEC(DoubleDelta, LZ4) TTL Date + INTERVAL 1 MONTH
 ) ENGINE = GraphiteMergeTree('graphite_rollup')
-PARTITION BY toYYYYMM(Date)
+PARTITION BY toYYYYMMDD(Date)
 ORDER BY (Path, Time);
 
 CREATE TABLE IF NOT EXISTS default.graphite_index (
-  Date Date,
+  Date Date CODEC(DoubleDelta, LZ4),
   Level UInt32,
-  Path String,
+  Path String CODEC(ZSTD(3)),
   Version UInt32
 ) ENGINE = ReplacingMergeTree(Version)
-PARTITION BY toYYYYMM(Date)
+PARTITION BY toYYYYMMDD(Date)
 ORDER BY (Level, Path, Date);
 
 CREATE TABLE IF NOT EXISTS default.graphite_tagged (
-  Date Date,
-  Tag1 String,
-  Path String,
-  Tags Array(String),
-  Version UInt32
+  Date Date CODEC(DoubleDelta, LZ4),
+  Tag1 String CODEC(ZSTD(3)),
+  Path String CODEC(ZSTD(3)),
+  Tags Array(String) CODEC(ZSTD(3)),
+  Version UInt32 TTL Date + INTERVAL 1 MONTH
 ) ENGINE = ReplacingMergeTree(Version)
-PARTITION BY toYYYYMM(Date)
+PARTITION BY toYYYYMMDD(Date)
 ORDER BY (Tag1, Path, Date);
